313358b3fa5ea3414c6cb6d2f53d1d01dfde8c64
Darren Segal <me@darren-segal.com>
2020-01-09 00:08:27 -0500

    skills

---
+++ src/components/Skill.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+  <div>
+    <p>{{ data.fields.name }}</p>
+    <div class="bar">
+      <div class="inner" :style="{width: bar}"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import anime from 'animejs'
+export default {
+  name: "Skill",
+  props: ['data'],
+  data() {
+    return {
+      width: 0
+    }
+  },
+  mounted() {
+    console.log(this.bar)
+    anime({
+      targets: {width: this.width},
+      width: [0,this.bar],
+      duration: 2000,
+      delay: 1000
+    })
+  },
+  computed: {
+    bar() {
+      return Math.round((this.data.fields.level / 5) * 100) + "%"
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/scss/variables";
+
+.bar {
+  height: 50px;
+  border: 2px solid $green;
+  margin-bottom: 48px;
+}
+
+p {
+  margin-bottom: 12px;
+}
+
+.inner {
+  height: 100%;
+  background-color: $green;
+}
+</style>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,22 +1,39 @@
 <template lang="html">
 		<div id="skills">
 			<h2>Skills</h2>
-			<p>JavaScript</p>
+			<Skill :key="entry.sys.id" v-for="entry of entries" :data="entry"/>
 		</div>
 </template>
 
 <script>
-	export default {
-		name: 'Skill'
-	}
+import client from "@/contentful";
+import Skill from "@/components/Skill"
+
+export default {
+	name: "Skills",
+	components: {Skill},
+	data() {
+		return {
+			entries: null
+		}
+	},
+  mounted() {
+    client
+      .getEntries({ content_type: "skill" })
+      .then(data => {
+        this.entries = data.items
+      })
+      .catch(err => console.log(err));
+  }
+};
 </script>
 
-<style lang="scss">
-	@import '~@/assets/scss/variables';
+<style lang="scss" scoped>
+@import "~@/assets/scss/variables";
 
-	#skills {
-		width: 100%;
-		height: 500px;
-		color: $green;
-	}
+#skills {
+  width: 100%;
+  height: 500px;
+  color: $green;
+}
 </style>
