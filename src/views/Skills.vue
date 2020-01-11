c83b6b56a28d8db578f7cd74eade8f8b49800981
Darren Segal <me@darren-segal.com>
2020-01-11 09:55:42 -0500

    v-for key

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,7 +1,7 @@
 <template lang="html">
 		<div id="skills">
 			<h2>Skills</h2>
-			<Skill v-for="entry of entries" :skill="entry"/>
+			<Skill v-for="entry of entries" :key="entry.sys.id" :skill="entry"/>
 		</div>
 </template>
 
@@ -19,11 +19,12 @@ export default {
 	},
   mounted() {
     client
-      .getEntries({ content_type: "skill", order: 'sys.createdAt' })
-      .then(data => {
+      .getEntries({
+        content_type: "skill",
+        order: 'sys.createdAt'
+      }).then(data => {
         this.entries = data.items
-      })
-      .catch(err => console.log(err));
+      }).catch(err => console.log(err));
   }
 };
 </script>
